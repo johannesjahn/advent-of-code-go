7be82d3c554f08fe35d0bd8af47fd7d3992290e4
Johannes Jahn <johannes.jahn@outlook.com>
2024-01-05 18:59:18 +0100

    add day 8 part 1

---
+++ 2023/day08/main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/alexchao26/advent-of-code-go/util"
+)
+
+//go:embed input.txt
+var input string
+
+func init() {
+	// do this in init (not main) so test file has same input
+	input = strings.TrimRight(input, "\n")
+	if len(input) == 0 {
+		panic("empty input.txt file")
+	}
+}
+
+func main() {
+	var part int
+	flag.IntVar(&part, "part", 1, "part 1 or 2")
+	flag.Parse()
+	fmt.Println("Running part", part)
+
+	if part == 1 {
+		ans := part1(input)
+		util.CopyToClipboard(fmt.Sprintf("%v", ans))
+		fmt.Println("Output:", ans)
+	} else {
+		ans := part2(input)
+		util.CopyToClipboard(fmt.Sprintf("%v", ans))
+		fmt.Println("Output:", ans)
+	}
+}
+
+type instruction struct {
+	op    string
+	left  string
+	right string
+}
+
+func parseLine(line string) instruction {
+	re := regexp.MustCompile(`(?m)(\S+) = \((\S+), (\S+)\)`).FindStringSubmatch(line)
+	return instruction{re[1], re[2], re[3]}
+}
+
+func part1(input string) int {
+	parsed := parseInput(input)
+	_ = parsed
+
+	instructions := parsed[0]
+
+	instructionMap := map[string]instruction{}
+
+	for i := 2; i < len(parsed); i++ {
+		instruction := parseLine(parsed[i])
+		instructionMap[instruction.op] = instruction
+	}
+
+	executionCount := 0
+	currentInstruction := instructionMap["AAA"]
+	currentInstructionIdx := 0
+
+	for true {
+		instruction := instructions[currentInstructionIdx]
+		if string(currentInstruction.op) == "ZZZ" {
+			return executionCount
+		}
+		executionCount++
+		if string(instruction) == "R" {
+			currentInstruction = instructionMap[currentInstruction.right]
+		} else {
+			currentInstruction = instructionMap[currentInstruction.left]
+		}
+		currentInstructionIdx = (currentInstructionIdx + 1) % len(instructions)
+	}
+
+	return 0
+}
+
+func part2(input string) int { // TODO: fix this
+	parsed := parseInput(input)
+	_ = parsed
+
+	instructions := parsed[0]
+
+	instructionMap := map[string]instruction{}
+
+	for i := 2; i < len(parsed); i++ {
+		instruction := parseLine(parsed[i])
+		instructionMap[instruction.op] = instruction
+	}
+
+	executionCount := 0
+	currentInstruction := instructionMap["AAA"]
+	currentInstructionIdx := 0
+
+	for true {
+		instruction := instructions[currentInstructionIdx]
+		if string(currentInstruction.op) == "ZZZ" {
+			return executionCount
+		}
+		executionCount++
+		if string(instruction) == "R" {
+			currentInstruction = instructionMap[currentInstruction.right]
+		} else {
+			currentInstruction = instructionMap[currentInstruction.left]
+		}
+		currentInstructionIdx = (currentInstructionIdx + 1) % len(instructions)
+	}
+
+	return 0
+}
+
+func parseInput(input string) (ans []string) {
+	for _, line := range strings.Split(input, "\n") {
+		ans = append(ans, line)
+	}
+	return strings.Split(input, "\n")
+}
